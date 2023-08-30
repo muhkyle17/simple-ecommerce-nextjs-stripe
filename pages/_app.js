ca31d1f5ea6c09e56adc5e51e66f9a2d5a014a2f
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-08-29 19:59:14 -0700

    Connect to stripe's cart provider API

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,27 @@
-import Layout from "@/components/Layout";
-import "@/styles/globals.css";
+import Layout from '@/components/Layout'
+import '@/styles/globals.css'
+import { CartProvider } from 'use-shopping-cart'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+    <CartProvider
+      mode='payment'
+      cartMode='client-only'
+      // Connect to stripe account
+      stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}
+      // Redirects here after successful payments
+      successUrl={`${process.env.NEXT_PUBLIC_URL}/success`}
+      // Redirects here when you click back on Stripe Checkout
+      cancelUrl={`${process.env.NEXT_PUBLIC_URL}/?success=false`}
+      currency='CAD'
+      // Only customer from Canada will be able to purchase
+      // Having this setting means that we will capture shipping address
+      allowedCountries={['CA']}
+      // Enables local storage
+      shouldPersist={true}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CartProvider>
+  )
 }
