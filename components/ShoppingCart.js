6cfadf5ab5a18f69061c85b62bfd51c8f5ede3cd
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-08-29 22:02:07 -0700

    Format each file according to the eslint of my local settings

--- components/ShoppingCart.js
+++ components/ShoppingCart.js
@@ -1,21 +1,22 @@
-import CartItem from "./CartItem";
-import CheckoutButton from "./CheckoutButton";
+import CartItem from './CartItem'
+import CheckoutButton from './CheckoutButton'
+import { useShoppingCart } from 'use-shopping-cart'
 
-const items = [];
+const items = []
 
 export default function ShoppingCart() {
   return (
-    <div className="bg-white flex flex-col absolute right-3 md:right-9 top-14 w-80 py-4 px-4 shadow-[0_5px_15px_0_rgba(0,0,0,.15)] rounded-md">
+    <div className='bg-white flex flex-col absolute right-3 md:right-9 top-14 w-80 py-4 px-4 shadow-[0_5px_15px_0_rgba(0,0,0,.15)] rounded-md'>
       {items.length > 0 ? (
         <>
-          {items.map((item) => (
+          {items.map(item => (
             <CartItem item={item} key={item.id} />
           ))}
           <CheckoutButton />
         </>
       ) : (
-        <div className="p-5">You have no items in your cart</div>
+        <div className='p-5'>You have no items in your cart</div>
       )}
     </div>
-  );
+  )
 }
