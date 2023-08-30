97d5dfeb5ffeb2d75a7727e2df2c24810e0cc19d
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-08-29 22:14:00 -0700

    Add the logic to display the shopping cart

--- components/ShoppingCart.js
+++ components/ShoppingCart.js
@@ -6,7 +6,10 @@ const items = []
 
 export default function ShoppingCart() {
   return (
-    <div className='bg-white flex flex-col absolute right-3 md:right-9 top-14 w-80 py-4 px-4 shadow-[0_5px_15px_0_rgba(0,0,0,.15)] rounded-md'>
+    <div
+      className={`bg-white flex flex-col absolute right-3 md:right-9 top-14 w-80 py-4 px-4 shadow-[0_5px_15px_0_rgba(0,0,0,.15)] rounded-md transition-opacity duration-500 ${
+        shouldDisplayCart ? 'opacity-100' : 'opacity-0'
+      }`}>
       {items.length > 0 ? (
         <>
           {items.map(item => (
